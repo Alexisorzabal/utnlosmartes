c4ed6b7e54f917aa9b7a8c290bd977921c066859
Alexisorzabal <106978463+Alexisorzabal@users.noreply.github.com>
2023-06-06 13:20:33 -0300

    error

---
+++ transportes/frontend/src/components/layout/Header.js
@@ -0,0 +1,7 @@
+import react from 'react';
+const Header =(props)=>{
+    return(
+    <div>Header</div>
+    );
+}
+export default Header;
---
+++ transportes/frontend/src/components/layout/Nav.js
@@ -0,0 +1,7 @@
+import react from 'react';
+const Nav =(props)=>{
+    return(
+    <div>Nav</div>
+    );
+}
+export default Nav;

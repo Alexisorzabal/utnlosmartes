119add2093b8b3448cb2b68b5ab6c67f94b0bfff
Alexisorzabal <106978463+Alexisorzabal@users.noreply.github.com>
2023-06-08 18:13:50 -0300

    funciona server

---
+++ transportes/front/front/src/pages/NosotrosPage.js
@@ -0,0 +1,43 @@
+import React from "react";
+const NosotrosPage = (props) => {
+    return (
+        <main className="holder">
+            <div className="historia">
+                <h2>Historia</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur
+                    adipisicing elit. Consequuntur, rem saepe. A
+                    enim qui facilis consectetur. Tempora dolores
+                    culpa libero a natus fugit e ratione
+                    reprehenderit,
+                    consequuntur, debitis corrupti asperiores.</p>
+                <p>Ipsum dolor sit, amet consectetur
+                    adipisicing elit. Consequuntur, rem saepe. A
+                    enim qui facilis consectetur. Tempora dolores
+                    culpa libero a natus fugit e ratione
+                    reprehenderit, consequuntur, debitis corrupti
+                    asperiores.</p>
+
+            </div>
+            <div className="staff">
+                <h2>Staff</h2>
+                <div className="personas">
+                    <div className="persona">
+                        <img src=" img/nosotros/nosotros1.jpg" alt="juan gomez" />
+
+                        <h5>Juan Gomez</h5>
+                        <h6>Gerente General</h6>
+                        <p>Lorem ipsum dolor sit amet,
+                            consectetur adipisicing elit. Obcecati
+                            aspernatur deleniti, endis mollitia
+                            voluptas est accusantium veritatis
+                            quidem, autem architecto commodi
+                            cumque, suscipit nobis molestias!
+                            Voluptatibus esse quibusdam sunt
+                            architecto.</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
+export default NosotrosPage;

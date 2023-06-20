3d94d87164b174acd8a5915db1d2666979fd0f36
Alexisorzabal <106978463+Alexisorzabal@users.noreply.github.com>
2023-06-20 08:43:34 -0300

    node

--- modulo4:u6/app.js
+++ modulo4:u6/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { send } = require('process');
 
 var app = express();
 
@@ -21,6 +22,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/prueba', function(req,res){
+  res.send('prueba')
+})
+app.get('/pruebados', function(req,res){
+  res.send('prueba dos')
+})
+app.get('/pruebatres', function(req,res){
+  res.send('prueba tres')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

245618929df87d7d5bdd531302543186c4619995
Alexisorzabal <106978463+Alexisorzabal@users.noreply.github.com>
2023-08-03 07:52:09 -0300

    error agregar

--- modulo4:4/routes/admin/novedades.js
+++ modulo4:4/routes/admin/novedades.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-router.get('/', function (req, res, next) {
+var novedadesModel = require('../../models/novedadesModel')
+
+router.get('/',async function (req, res, next) {
+    
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        persona : req.session.nombre
+        persona : req.session.nombre,
+        novedades
     });
 });
+router.get('/agregar', (req,res,next) => {
+    res.render('admin/agregar',{
+        layout:'admin/layout'
+})
+});
+
 module.exports = router;

6c88e107148e7e2e28a0d6267a74ecf2ce5f75ea
Alexisorzabal <106978463+Alexisorzabal@users.noreply.github.com>
2023-08-15 14:27:48 -0300

    api error

--- modulo4:4/app.js
+++ modulo4:4/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fileUpload = require('express-fileupload');
+var cors = require('cors');
 
 
 require('dotenv').config();
@@ -13,6 +14,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
 var adminRouter = require('./routes/admin/novedades');
+var apiRouter = require('./routes/api');
+
 var app = express();
 
 // view engine setup
@@ -66,7 +69,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
 app.use('/admin/novedades', secured, adminRouter);
-// catch 404 and forward to error handler
+app.use('/api', cors(),apiRouter);
 app.use(function (req, res, next) {
   next(createError(404));
 });
